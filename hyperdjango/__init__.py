4566f773f27654816f6ecfe7a8e3dca21cfe5111
Waseem Akram <waseem07799@gmail.com>
2026-04-14 09:55:50 +0530

    feat: add default_app_config

--- hyperdjango/__init__.py
+++ hyperdjango/__init__.py
@@ -2,6 +2,8 @@ from hyperdjango.actions import Actions, Event, action
 from hyperdjango.page import HyperActionMixin, HyperPageTemplate, HyperView, Page
 from hyperdjango.shortcuts import render_template_block, render_template_page
 
+default_app_config = "hyperdjango.apps.HyperDjangoConfig"
+
 __all__ = [
     "HyperPageTemplate",
     "HyperActionMixin",
