bcdd064a327633fa97c09831a46d7c48ddbc2758
Waseem Akram <waseem07799@gmail.com>
2026-04-28 00:04:11 +0530

    feat(website): generate favicon and social preview assets

--- website/config/settings.py
+++ website/config/settings.py
@@ -71,7 +71,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.getenv("STATIC_ROOT", str(BASE_DIR / "staticfiles"))
-STATICFILES_DIRS = [BASE_DIR / "dist"]
+STATICFILES_DIRS = [BASE_DIR / "dist", BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
